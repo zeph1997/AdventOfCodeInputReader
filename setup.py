88dc22320c02a14cd515cc015a6751b1ffd6e3b3
zeph1997 <zeph1997@hotmail.com>
2021-12-03 17:45:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from distutils.core import setup
+from io import open
+
+def read(filename):
+    with open(filename, encoding='utf-8') as file:
+        return file.read()
+
 setup(
   name = 'AdventOfCodeInputReader',         
   packages = ['AdventOfCodeInputReader'],   
-  version = '0.0.1',      
+  version = '0.0.2',      
   license='MIT',        
   description = 'Package to retrieve input for Advent Of Code',   
+  long_description=read('README.md'),
+  long_description_content_type="text/markdown",
   author = 'Zeph Ng',                   
   author_email = 'zephngdev@gmail.com',      
   url = 'https://github.com/zeph1997/AdventOfCodeInputReader',   
-  download_url = 'https://github.com/zeph1997/AdventOfCodeInputReader/archive/refs/tags/v0.0.1.tar.gz',    
+  download_url = 'https://github.com/zeph1997/AdventOfCodeInputReader/archive/refs/tags/v0.0.2.tar.gz',    
   keywords = ['reader', 'advent', 'of', 'code', 'api'],   
   install_requires=['requests'],
   classifiers=[
